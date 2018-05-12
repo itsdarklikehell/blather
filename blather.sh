37123d0a1025598251cd53eb7a47322731ffe159
Bauke <bauke.molenaar@gmail.com>
2018-05-12 22:33:14 +0200

    modified:   Blather-Installer 	modified:   Recognizer.pyc 	modified:   blather.sh 	modified:   config/language/lm 	modified:   config/sentences.corpus

--- blather.sh
+++ blather.sh
@@ -8,14 +8,14 @@ sleep .1
 # set some shortcuts to use in the commands file
 
 #export VOICE="/usr/bin/festival --tts"
-#export VOICE="/usr/bin/espeak"
 export VOICE="/usr/bin/flite"
+#export VOICE="/usr/bin/espeak"
 sleep .1
-export PLUGINS="$HOME/blather/config/plugins"
+export PLUGINS="/home/pi/blather/config/plugins"
 sleep .1
-export CONFIGDIR="$HOME/blather/config"
+export CONFIGDIR="/home/pi/blather/config"
 sleep .1
-export CLIP="$HOME/.local/share/clipit/history"
+export CLIP="/home/pi/.local/share/clipit/history"
 sleep .1
 # export KEYPRESS="xvkbd -xsendevent -secure -text"
 export KEYPRESS="xdotool key"
@@ -30,17 +30,21 @@ sleep .1
 export CLICK="xdotool click"
 # Generally, left = 1, middle = 2, right = 3, wheel up = 4, wheel down = 5
 sleep .1
-export BROWSER="chromiun-browser"
+export BROWSER="firefox"
 sleep .1
-export CHBROWSER="chromium-browser"
+export CHBROWSER="google-chrome"
+sleep .1
+export CRMBROWSER="chromium-browser"
 sleep .1
 export EDITOR="geany"
+#export EDITOR="atom"
 sleep .1
-#export FM="thunar"
 export FM="pcmanfm"
+#export FM="thunar"
 sleep .3
 
 # start blather in continuous mode with the GTK GUI
 # and a history of 20 recent commands
-bash $HOME/blather/language_updater.sh
-python $HOME/blather/Blather.py
+/home/pi/blather/./language_updater.sh
+python2 /home/pi/blather/Blather.py
+
